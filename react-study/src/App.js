8dcf7202ae36ed16919de07eeafbd04a2b3ca4e0
Luke_YS <yjsmk0902@gmail.com>
2023-03-14 11:41:31 +0900

    useEffect

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,11 +1,31 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => console.log("처음 한번 렌더링"), []);
+  useEffect(() => console.log("counter 로직 호출"), [counter]);
+  useEffect(() => {
+    if (keyword !== null && keyword.length >= 5)
+      console.log("Search 로직 호출 : " + keyword);
+  }, [keyword]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Button"} />
+      <input
+        value={keyword}
+        type="text"
+        placeholder="검색..."
+        onChange={onChange}
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <Button text={"Button"} onClick={onClick} />
     </div>
   );
 }
