899b1b8187821566f3dd8c4f07deb16cfc7ce45c
Luke_YS <yjsmk0902@gmail.com>
2023-03-15 15:12:04 +0900

    Coin Translator

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -3,6 +3,14 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [monetary, setMonetary] = useState("");
+  const [price, setPrice] = useState(1);
+
+  const selected = (event) => {
+    setPrice(event.target.value);
+  };
+  const entered = (event) => setMonetary(event.target.value);
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((Response) => Response.json())
@@ -13,15 +21,29 @@ function App() {
   }, []);
   return (
     <div>
-      <h1>COINS ({coins.length})</h1>
-      {loading ? <strong>로딩중.....</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li key={coin.id}>
-            {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
-          </li>
-        ))}
-      </ul>
+      <h1>코인 단위 환산기</h1>
+      {loading ? (
+        <strong>로딩중.....</strong>
+      ) : (
+        <div>
+          <select onChange={selected}>
+            <option value="1">코인을 선택하세요 </option>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name}({coin.symbol})
+              </option>
+            ))}
+          </select>
+          <hr />
+          <input
+            onChange={entered}
+            type="number"
+            placeholder="단위를 입력하세요"
+            value={monetary}
+          ></input>
+          {monetary === "" ? null : <h1>{price * monetary}$</h1>}
+        </div>
+      )}
     </div>
   );
 }
